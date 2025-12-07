1d9324aa951a1aa2390efd174cec71e188f1ef08
abdullahtahir001100 <abdullahtahi001@gmail.com>
2025-12-07 12:12:14 -0200

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.set('trust proxy', 1); // Required for secure cookies (and getting real IP f
 const corsOptions = {
     // Development mein, request bhejne wale origin ko hi echo back karein
     // Kyunki credentials: true ke saath origin: '*' use nahi ho sakta
-    origin: "https://website-kappa-woad.vercel.app",
+    origin: "https://www.javehandmade.store",
     credentials: true, // Allow cookies to be sent/received
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
