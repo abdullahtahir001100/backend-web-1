d4f0b8c564bbcd826363d4ffad3c05b1f1484288
Abdullah Tahir <abdullahtahi001@gmail.com>
2026-03-26 21:30:56 +0500

    Create OTP.js

---
+++ models/OTP.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const OTPSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    otp: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, expires: 600 } // 10 minutes mein auto-delete
+});
+
+module.exports = mongoose.model('OTP', OTPSchema);
